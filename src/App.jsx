b55750cbbef2ffe42d0a06c0b71ae160bac0957a
pinkpagejpeg <isip_a.a.stolbova@mpt.ru>
2023-11-17 12:08:24 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+      </div>
+    </div>
+  )
+}
+
+export default App
